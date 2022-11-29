414ef750220d18cecd58f1e1ab0448963c9a479f
Xavier DMello <xaviermdmello@gmail.com>
2022-11-29 15:13:05 -0500

    even more refactoring

--- contracts/BorrowOptimizer.sol
+++ contracts/BorrowOptimizer.sol
@@ -1,9 +1,10 @@
 pragma solidity ^0.8.0;
 
 import {BaseVault} from "./BaseVault.sol";
+import {IBaseLender} from "./lenders/IBaseLender.sol";
 
 /// @author Xavier D'Mello www.xavierdmello.com
-contract BorrowOptimizer is BaseVault{
+contract BorrowOptimizer is BaseVault, IBaseLender {
     function beforeRepay() internal virtual {}
     function afterBorrow() internal virtual {}
 }
--- contracts/implementations/KSMMoonwellLidoBeefy.sol
+++ contracts/implementations/KSMMoonwellLidoBeefy.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.8.0;
 
 import {BorrowOptimizer} from "../BorrowOptimizer.sol";
+import {MoonwellLender} from "../lenders/MoonwellLender.sol";
 
-contract KSMMoonwellLidoBeefy is BorrowOptimizer {
+contract KSMMoonwellLidoBeefy is BorrowOptimizer, MoonwellLender {
     function beforeRepay() internal override {}
     function afterBorrow() internal override {}
     function compound() public {}
