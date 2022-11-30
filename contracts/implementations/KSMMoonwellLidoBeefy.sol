5d4137daec7d4c8f0b15389ff3d9caea2a45a077
Xavier DMello <xaviermdmello@gmail.com>
2022-11-29 19:16:40 -0500

    add comment

--- contracts/implementations/KSMMoonwellLidoBeefy.sol
+++ contracts/implementations/KSMMoonwellLidoBeefy.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.8.0;
 
 import {BorrowOptimizer} from "../BorrowOptimizer.sol";
 import {MoonwellLender} from "../lenders/MoonwellLender.sol";
-
+// TODO: Implement sandwich attack protection
 contract KSMMoonwellLidoBeefy is BorrowOptimizer, MoonwellLender {
     function beforeRepay() internal override {}
     function afterBorrow() internal override {}
